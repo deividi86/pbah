b258d34476a38a69f9478e0e2d9ce8717c3ef873
Deividi Silva <deividi@gmail.com>
2015-04-25 21:25:51 -0700

    parse response

--- src/app.js
+++ src/app.js
@@ -6,12 +6,8 @@ var card = new UI.Card({
   title:'AngelHack Timeline'
 });
 
-// Display the Card
-card.show();
-
 // Construct URL
-var key = 'key';
-var URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=angelhack&count=1' + key;
+var URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=angelhack&count=1';
 
 // Make the request every minute
 setInterval(function(){
@@ -20,22 +16,21 @@ setInterval(function(){
       url: URL,
       type: 'json',
       crossDomain: true,
-      headers: { 'Authorization': 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1429997754",oauth_nonce="2739203158",oauth_version="1.0",oauth_token="21090358-RquisURH9ffbUXhhbCGfuvEzNWqfy9DakiFiphWQN",oauth_signature="ZqdQRd20o8wFlmAqjiFOU33wzDQ%3D"'}
+      headers: { 'Authorization': 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1430021176",oauth_nonce="2019906746",oauth_version="1.0",oauth_token="21090358-RquisURH9ffbUXhhbCGfuvEzNWqfy9DakiFiphWQN",oauth_signature="RmWAWhGIyT3t45bdjBfM1bI%2FCCo%3D"'}
     },
     function(data) {
       // Success!
       console.log("Successfully fetched twitter timeline!");
-      console.log(data.statuses[0].text);
-  
-      // Always upper-case first letter of description
-      var description = data.statuses[0].text;
-      description = description.charAt(0).toUpperCase() + description.substring(1);
-  
+      var tweet = JSON.stringify(data).replace('statuses: ','');
+      tweet.replace('[','');
+      tweet.replace(']','');
       // Show to user
-      card.body(description);
+      card.body(JSON.parse(tweet).text);
     },
     function(error) {
       console.log('Failed fetching twitter timeline: ' + error);
     }
   );
-}, 1000 * 15);
+  // Display the Card
+  card.show();
+}, 1000 * 5);
