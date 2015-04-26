a0b8d57aae31604d73916fa418e984825fc6f734
Deividi Silva <deividi@gmail.com>
2015-04-25 22:04:11 -0700

    changed api call

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var card = new UI.Card({
 });
 
 // Construct URL
-var URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=angelhack&count=1';
+var URL = 'https://api.twitter.com/1.1/search/tweets.json?q=%40AngelHack';
 
 // Make the request every minute
 setInterval(function(){
@@ -16,16 +16,14 @@ setInterval(function(){
       url: URL,
       type: 'json',
       crossDomain: true,
-      headers: { 'Authorization': 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1430021176",oauth_nonce="2019906746",oauth_version="1.0",oauth_token="21090358-RquisURH9ffbUXhhbCGfuvEzNWqfy9DakiFiphWQN",oauth_signature="RmWAWhGIyT3t45bdjBfM1bI%2FCCo%3D"'}
+      headers: { 'Authorization': 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1430024339",oauth_nonce="1330515815",oauth_version="1.0",oauth_token="21090358-RquisURH9ffbUXhhbCGfuvEzNWqfy9DakiFiphWQN",oauth_signature="AXXNIKeDtljwMplgvZX3cWgwUOI%3D"'}
     },
     function(data) {
       // Success!
       console.log("Successfully fetched twitter timeline!");
-      var tweet = JSON.stringify(data).replace('statuses: ','');
-      tweet.replace('[','');
-      tweet.replace(']','');
+      var tweet = data.statuses[0].text;
       // Show to user
-      card.body(JSON.parse(tweet).text);
+      card.body(tweet);
     },
     function(error) {
       console.log('Failed fetching twitter timeline: ' + error);
